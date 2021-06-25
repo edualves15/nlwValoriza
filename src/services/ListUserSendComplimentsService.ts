60f4e7e8832267baccc5202a99ebc08819e6f517
edualves15 <edualves15@gmail.com>
2021-06-25 11:57:12 -0300

    Aula 05

---
+++ src/services/ListUserSendComplimentsService.ts
@@ -0,0 +1,22 @@
+import { getCustomRepository } from "typeorm";
+import { ComplimentsRepository } from "../repositories/ComplimentsRepositories";
+
+
+class ListUserSendComplimentsService {
+    async execute(user_id: string) {
+        const complimentsRepositories = getCustomRepository(
+            ComplimentsRepository
+        );
+
+        const compliments = await complimentsRepositories.find({
+            where: {
+                user_sender: user_id
+            },
+            // relations: ["userSender", "userReceiver", "tag"],
+        });
+
+        return compliments;
+    }
+}
+
+export { ListUserSendComplimentsService };
